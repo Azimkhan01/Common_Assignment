f5adeac00e64dfa2dfb0cfb2136d8ad466f1080b
Azimkhan01 <azimkarimrahim@gmail.com>
2025-03-18 17:02:54 +0530

    some new

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,11 @@ import React, { useEffect, useState } from "react";
 import * as XLSX from "xlsx";
 import Self from "./component/Self";
 import Same from "./component/Same";
-const ExcelReader = () => {
-  const [data, setData] = useState([]);
-  const [roll,setRoll] = useState("")
-  useEffect(() => {
-    // Replace 'sample.xlsx' with your actual file name
-    fetch("/public/Copy of CCF_Assignment_24-25(1).xlsx")
-      .then((response) => response.arrayBuffer())
-      .then((buffer) => {
-        const workbook = XLSX.read(buffer, { type: "array" });
-        const sheetName = workbook.SheetNames[0];
-        const sheet = workbook.Sheets[sheetName];
-        const jsonData = XLSX.utils.sheet_to_json(sheet);
-        setData(jsonData);
-      })
-      .catch((error) => console.error("Error loading the Excel file:", error));
-  }, []);
+import data from './controller/output.js'
+const App = () => {
+  const [dat] = useState(data);
+  const [roll,setRoll] = useState(0)
+ 
 
   return (
     <div className="p-4 w-[100%]">
@@ -31,14 +20,14 @@ const ExcelReader = () => {
       </div>
 
       {
-        (roll.length == 6) ? <Self data={data} roll={roll} /> :<h1 className="underline text-red-400">Enter proper Roll Number</h1>
+        (roll.length == 6) ? <Self data={dat} roll={roll} /> :<h1 className="underline text-red-400">Enter proper Roll Number</h1>
       }
       {
-        (roll.length == 6) ? <Same data={data} roll={roll} /> : <h1 className="underline text-red-400">For Detail Enter the Roll Number.</h1>
+        (roll.length == 6) ? <Same data={dat} roll={roll} /> : <h1 className="underline text-red-400">For Detail Enter the Roll Number.</h1>
       }
       
     </div>
   );
 };
 
-export default ExcelReader;
+export default App;
